a4105a0e91c8ef9acc7893f93a03dc45f9c2b50d
Tyson George <tyson.george.5nrag@aleeas.com>
2024-01-09 16:57:25 -0500

    updating keymap

--- code.py
+++ code.py
@@ -1,9 +1,7 @@
-import board
-
 from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.modules.split import Split, SplitSide
+from kmk.modules.split import Split
 from kmk.modules.holdtap import HoldTap
 from kmk.extensions.media_keys import MediaKeys
 
@@ -21,41 +19,39 @@ keyboard.modules.append(HoldTap())
 keyboard.modules.append(MediaKeys())
 
 # settings up layers
-MOMENTARY = KC.MO(1) 
+MOMENTARY = KC.MO(1)
 MOD_LAYER = KC.LM(1, KC.LSFT)
-MOD_LAYER2 = KC.LM(0, KC.LALT)
-LAYER_TAP = KC.LT(1, KC.DEL, prefer_hold=True, tap_interrupted=False, tap_time=250) # tap longer than 250ms to trigger
-RCS = KC.HT(KC.RGUI, KC.RCTRL(KC.RSFT))
+RCS = KC.HT(KC.RCTRL, KC.RSFT(KC.RCTRL), tap_time=250, prefer_hold=False)
+LCG = KC.HT(KC.LALT, KC.LGUI(KC.LCTRL), tap_time=250, prefer_hold=False)
 
 # for vim/kakoune
-A = KC.HT(KC.A, KC.LSFT(KC.A), prefer_hold=False, tap_interrupted=True)
-I = KC.HT(KC.I, KC.LSFT(KC.I), prefer_hold=False, tap_interrupted=True)
+# A = KC.HT(KC.A, KC.LSFT(KC.A), prefer_hold=False, tap_interrupted=True)
+# I = KC.HT(KC.I, KC.LSFT(KC.I), prefer_hold=False, tap_interrupted=True)
 
 # Modified Workman Layout (US) 
 keyboard.keymap = [
     [   # main layer
-        KC.GRAVE,  KC.N1,   KC.N2,   KC.N3, KC.TRNS, KC.TRNS,                           KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,     MOD_LAYER2,
+        KC.GRAVE,  KC.N1,   KC.N2,   KC.N3, KC.TRNS, KC.TRNS,                           KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,     KC.EQL,
         KC.TAB,    KC.Q,    KC.D,    KC.R,  KC.W,    KC.B,                              KC.J,  KC.F,    KC.U,    KC.P,    KC.SCLN,   KC.BSLS,
-        KC.ESC,    A,       KC.S,    KC.H,  KC.T,    KC.G,                              KC.Y,  KC.N,    KC.E,    KC.O,    I,         KC.QUOT,
-        KC.LSFT,   KC.Z,    KC.X,    KC.M,  KC.C,    KC.V,                              KC.K,  KC.L,    KC.COMM, KC.DOT,  KC.TRNS,   KC.SLSH,
-                        KC.LPRN, KC.LBRACKET,                                                           KC.RPRN, KC.RBRACKET,
-                                               KC.LGUI,  KC.SPC,         KC.ENT,    KC.BSPC,
-                                               KC.LCTRL, KC.TRNS,        LAYER_TAP, KC.LALT,
-                                               KC.HOME,  MOD_LAYER,      RCS,       KC.END
+        KC.ESC,    KC.A,    KC.S,    KC.H,  KC.T,    KC.G,                              KC.Y,  KC.N,    KC.E,    KC.O,    KC.I,      KC.QUOT,
+        KC.LSFT,   KC.Z,    KC.X,    KC.M,  KC.C,    KC.V,                              KC.K,  KC.L,    KC.COMM, KC.DOT,  KC.SLSH,   KC.MINUS,
+                        KC.LPRN, KC.LBRACKET,                                                           KC.RBRACKET, KC.RPRN,
+                                               KC.LGUI,  KC.SPC,        KC.ENT,    KC.BSPC,
+                                               KC.LCTRL, KC.NO,         MOMENTARY, KC.RSFT,
+                                               KC.HOME,  LCG,           RCS,    KC.END
     ], 
 
     [   # num layer
-        KC.NO,   KC.NO,    KC.NO,    KC.NO,     KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.NO,   KC.NO,    KC.NO,   KC.TRNS,
-        KC.NO,   KC.NO,    KC.NO,    KC.NO,     KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.MPRV, KC.MPLY,  KC.MNXT, KC.BSLS,
-        KC.ESC,  KC.N1,    KC.N2,    KC.N3,     KC.N4,  KC.N5,                              KC.N6,  KC.N7,    KC.N8,   KC.N9,    KC.N0,   KC.PMNS,
-        KC.TRNS, KC.NO,    KC.EQL,   KC.MINUS,  KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.RALT, KC.NO,    KC.NO,   KC.NO,
+        KC.NO,    KC.NO,    KC.NO,    KC.NO,     KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.NO,   KC.NO,    KC.NO,   KC.TRNS,
+        KC.TRNS,  KC.NO,    KC.NO,    KC.NO,     KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.MPRV, KC.MPLY,  KC.MNXT, KC.BSLS,
+        KC.TRNS,  KC.N1,    KC.N2,    KC.N3,     KC.N4,  KC.N5,                              KC.N6,  KC.N7,    KC.N8,   KC.N9,    KC.N0,   KC.PMNS,
+        KC.TRNS,  KC.NO,    KC.EQL,   KC.MINUS,  KC.NO,  KC.NO,                              KC.NO,  KC.NO,    KC.TRNS, KC.TRNS,  KC.TRNS, KC.NO,
                         KC.UP, KC.DOWN,                                                           KC.LEFT, KC.RIGHT,
-                                              KC.TRNS,     KC.TRNS,         KC.ENT,   KC.BSPC,
-                                              KC.TRNS,     KC.TRNS,            KC.TRNS,  KC.TRNS,
+                                              KC.TRNS,     KC.TRNS,         KC.TRNS,   KC.TRNS,
+                                              KC.TRNS,     KC.TRNS,         KC.TRNS,  KC.TRNS,
                                               KC.TRNS,     KC.TRNS,         KC.TRNS,  KC.TRNS
     ]
 ]
 
 if __name__ == '__main__':
     keyboard.go()
-
